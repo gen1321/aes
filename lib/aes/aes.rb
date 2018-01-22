7c47f6ddd1fd99af3c351383338f1637b6cd9a91
gen <gen1321@mail.ru>
2018-01-22 13:56:17 +0400

    remove key modifier

--- lib/aes/aes.rb
+++ lib/aes/aes.rb
@@ -152,7 +152,6 @@ module AES
         # Toggles encryption mode
         @cipher.send(action)
         @cipher.padding = @options[:padding]
-        @cipher.key = @key.unpack('a2'*32).map{|x| x.hex}.pack('c'*32)
       end
   end
 end
